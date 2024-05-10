22f7b3b4675039eee56cbfe7f19eb22b0af97c0b
Dan Carlton <danlouis.carlton@gmail.com>
2024-05-09 18:57:43 -0700

    phonebook exercises 3.1-3.6

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { error } = require( 'console' )
 const express = require('express')
 const app = express()
 
@@ -5,7 +6,7 @@ app.use(express.json())
 
 
 
-[
+let persons = [
     {
       "id": 1,
       "name": "Arto Hellas",
@@ -32,8 +33,77 @@ app.get('/', (request, response) => {
   response.send('<h1>Its going to be ok, just keep grinding!</h1>')
 })
 
+
+// persons API calls
 app.get('/api/persons', (request, response) => {
-  const body = request.body
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if(person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  persons = persons.filter(person => person.id !== id);
+  response.status(204).end(); // No content to send back
+});
+
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+// add a person
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number is missing'
+    });
+  }
+
+  const exists = persons.some(person => person.name === body.name)
+  if(exists){
+    return response.status(404).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+
+
+// info API calls
+
+app.get('/info', (request, response) => {
+  const date = new Date()
+  response.send(`
+  <p>Phonebook has info for ${persons.length} people</p>
+  <br/>
+  <p>${date}</p>
+`);
 })
 
 
@@ -42,5 +112,3 @@ const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-// test
